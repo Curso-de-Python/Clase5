44765ea4e18cd9cba01b5d531725263920b71886
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-10-27 13:45:30 -0500

    Create ejercicio3.py

---
+++ ejercicio3.py
@@ -0,0 +1,23 @@
+'''
+-----------------------------
+ EJERCICIO N°3
+ Métodos de listas
+-----------------------------
+'''
+# MÉTODO .append()
+miLista1 = [] # creando una lista vacía
+
+for i in range (5):
+  miLista1.append (i + 1)
+
+print(miLista1)
+
+'''
+# MÉTODO .insert()
+miLista2 = [] # creando una lista vacía
+
+for i in range(5):
+  miLista2.insert(0, i + 1)
+
+print(miLista2)
+'''
