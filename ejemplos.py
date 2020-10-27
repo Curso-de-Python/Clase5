2593a03f6cfa5a0e4fbb68127df4373bca9ae713
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-10-27 13:44:29 -0500

    Create ejemplos.py

---
+++ ejemplos.py
@@ -0,0 +1,31 @@
+# OPERADORES BITWISE
+
+i = 15    # ...01111 en binario
+j = 22    # ...10110 en binario
+
+log = i and j
+bit = i & j
+
+print("log: ", log)
+print("bit: ", i & j)
+
+#logneg = not i
+#bitneg = ~ i
+
+#print("logneg: ", logneg)
+#print("bitneg: ", bitneg)
+
+# --------------------
+# SHIFTING
+
+#var = 17
+#varRight = var >> 1
+#varLeft = var << 2
+#print(var, varLeft, varRight)
+
+# --------------------
+# ÍNDICES NEGATIVOS
+
+#numeros = [111, 7, 2, 1]
+#print(numeros[-1])
+#print(numeros[-2])
