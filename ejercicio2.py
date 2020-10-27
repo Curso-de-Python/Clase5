cb3dd6ccf5dc117b43cb78d7009b731b0819eb54
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-10-27 13:45:08 -0500

    Create ejercicio2.py

---
+++ ejercicio2.py
@@ -0,0 +1,19 @@
+'''
+-----------------------------
+ EJERCICIO N°2
+ Métodos de listas
+-----------------------------
+'''
+numeros = [111, 7, 2, 1]
+print(len(numeros))
+print(numeros)
+
+# Método .append()
+numeros.append(4)
+print(len(numeros))
+print(numeros)
+
+# Método .insert()
+numeros.insert(0,222)
+print(len(numeros))
+print(numeros)
